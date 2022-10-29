fd8353d0651d12f01056edaef89ea2085a8d936c
Javier Sáez García <javiersaez1205@gmail.com>
2022-10-29 13:26:24 +0200

    fin previo

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -7,10 +11,46 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class VerPaisComponent implements OnInit {
+  // con la admitracion es patra que typescript confie en que el resultado puede ser nulo aunque siempre tendra data
+  pais!:Country[];
+  Object = Object;
 
-  constructor() { }
+  constructor( 
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisService
+    ) { }
+
 
   ngOnInit(): void {
+    // otra forma con RxJs switchMap
+    this.activatedRoute.params
+      .pipe(
+          switchMap(({id})  => this.paisService.getPaisPorCodigo(id)),
+          tap( console.log )        
+      )
+      .subscribe( pais => this.pais = pais); 
+
+
+    // esta forma es un subscribe dentro de otro subscribe que funciona igual que la anterior
+    // this.activatedRoute.params
+    //   .subscribe( ({id}) =>{
+    //       console.log(id);
+
+    //       this.paisService.getPaisPorCodigo( id )
+    //         .subscribe( pais => {
+    //           console.log(pais);
+            
+
+    //       })
+    //     }
+        
+    //   )
+    // 
+    
+    
+
+      
+
   }
 
 }
