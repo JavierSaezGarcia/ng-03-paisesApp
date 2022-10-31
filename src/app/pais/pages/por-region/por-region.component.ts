349b705a5e7800408acd355c9f4a556f21c4a1f1
Javier Sáez García <javiersaez1205@gmail.com>
2022-10-31 12:23:16 +0100

    por continente

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { ConnectableObservable, Observable } from 'rxjs';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
+  styles: [`
+    button {
+      margin-right: 5px;
+    }
+  `
   ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
+  
+  termino: string = '';
+  hayError: boolean = false;
+  //regiones: string[] = ['EU','EFTA','CARICOM','PA','AU','USAN','EEU','AL','ASEAN','CAIS','CEFTA','NAFTA','SAARC'];
+  regiones: string[] = ['europe','asia','africa','americas','oceania'];
+  paises: Country[] = [];
+  regionActiva: string = '';
+  placeholder:string = "Buscar continente...";
+  
 
-  constructor() { }
+  constructor( private paisService: PaisService) { }
 
-  ngOnInit(): void {
+  getClaseCSS( region: string): string {
+    return (region === this.regionActiva) 
+              ? 'btn btn-primary'
+              : 'btn btn-outline-primary';
   }
+
+  activarRegion ( region: string) {
+    if( region === this.regionActiva) { return;}
+    this.regionActiva = region;
+    this.paises = [];
+   
+    // if ( region === this.regionActiva ) { return; }
+ 
+    // this.regionActiva = region;
+    // this.paises = [];
+ 
+    this.paisService.buscarRegion( region )
+      .subscribe( {
+              next: paises => {
+                console.log( paises); 
+                this.paises = paises;
+              
+               
+          }
+        });
+      
+  }
+    
+  
+  
 
 }
